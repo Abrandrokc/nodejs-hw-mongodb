4191db630aa14a57baf06ed1ea17beed3c2e6d04
Abrandrokc <Abrandrokco@gmail.com>
2024-07-28 12:46:35 +0300

    aa

--- src/routers/auth-router.js
+++ src/routers/auth-router.js
@@ -5,11 +5,11 @@ import validateBody from "../middleware/validateBody.js";
 import { refreshController, signinController, signoutController, signupController } from "../controllers/auth.js";
 const authRouter = Router();
 
-authRouter.post("/signup", validateBody(userSignupSchema), ctrlWrapper(signupController));
+authRouter.post("/register", validateBody(userSignupSchema), ctrlWrapper(signupController));
 
-authRouter.post("/signin", validateBody(userSigninSchema), ctrlWrapper(signinController))
+authRouter.post("/login", validateBody(userSigninSchema), ctrlWrapper(signinController))
 
 authRouter.post("/refresh", ctrlWrapper(refreshController));
 
-authRouter.post("/signout", ctrlWrapper(signoutController))
+authRouter.post("/logout", ctrlWrapper(signoutController))
 export default authRouter;
